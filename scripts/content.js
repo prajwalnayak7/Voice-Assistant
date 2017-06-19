95ddf19516ec502d1807d4b6434c33816ef20919
prajwal <prajwalnayak7@gmail.com>
2017-06-19 12:00:26 +0300

    initial commit
    v 0.1.1

---
+++ scripts/content.js
@@ -0,0 +1,18 @@
+	if(document.title)
+	speak(' welcome to '+document.title);
+	
+	$(document).focusin(function(e) {
+	speak(e.target.text);
+	});
+		
+	function speak(txt){
+	var synth = window.speechSynthesis;
+	var voices = synth.getVoices();
+	if(true){
+	var utterThis = new SpeechSynthesisUtterance(txt);
+	utterThis.voice=voices[0];
+	utterThis.pitch = 1;
+	utterThis.rate = 1;
+	synth.speak(utterThis);
+	}
+	}
---
+++ scripts/popup.js
@@ -0,0 +1,11 @@
+
+
+window.onload = function() {
+  chrome.windows.getCurrent(function (currentWindow) {
+    chrome.tabs.query({active: true, windowId: currentWindow.id},
+                      function(activeTabs) {
+      chrome.tabs.executeScript(
+        activeTabs[0].id, {file: 'content.js', allFrames: true});
+    });
+  });
+};
